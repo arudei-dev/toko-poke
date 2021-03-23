6a2ddae07647ff622e11d66329163af24ed1dd34
hello-wahyu <al.arudei01@gmail.com>
2021-03-23 14:00:39 +0700

    Impl'd. base page component

---
+++ src/components/PageBase.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+interface Props {
+  children?: React.ReactNode,
+}
+
+const PageBase: React.FC<Props> = ({ children }) => {
+  const PageCss = css`
+    background-color: #e0e0e0;
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+  `;
+
+  return (
+    <div css={PageCss}>
+      { children }
+    </div>
+  )
+}
+
+
+
+export { PageBase }
