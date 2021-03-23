f7b3dcc1b992e7c94fa06abca1169ded17dd2cea
hello-wahyu <al.arudei01@gmail.com>
2021-03-23 20:42:21 +0700

    Injected Context Provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { RootStateProvider } from 'core/rootContext'
 import { MyPokemonLists } from './pages/MyPokemonLists';
 import { PokemonDetails } from './pages/PokemonDetails';
 import { PokemonLists } from './pages/PokemonLists';
@@ -8,13 +9,15 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact              component={PokemonLists}/>
-          <Route path="/pokemon/:id/details" component={PokemonDetails}/>
-          <Route path="/profile/poke-lists"  component={MyPokemonLists}/>
-       </Switch>
-      </Router>
+      <RootStateProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact              component={PokemonLists}/>
+            <Route path="/pokemon/:id/details" component={PokemonDetails}/>
+            <Route path="/profile/poke-lists"  component={MyPokemonLists}/>
+          </Switch>
+        </Router>
+      </RootStateProvider>
     </div>
   );
 }
