c5a02d54eb735a609067c938e45382229478847f
hello-wahyu <al.arudei01@gmail.com>
2021-03-24 20:44:55 +0700

    Refactored CardView's css into separate file and more

---
+++ src/components/CardView.style.tsx
@@ -0,0 +1,42 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+const cssCardViewHover = css`
+  &:hover {
+    box-shadow:  10px  10px 20px #bebebe,
+                -10px -10px 20px #ffffff;
+  }
+`
+
+const cssCardViewActive = css`
+  &:active:not(:focus-within) {
+    box-shadow: inset  7px  7px 14px #bebebe,
+                inset -7px -7px 14px #ffffff;
+  }
+`
+
+export type CssCardViewProps = {
+  isClickable: boolean,
+  isDisabled: boolean,
+}
+
+export const cssCardView = (props: CssCardViewProps) => css`
+  width: 100%;
+  min-height: 100px;
+  padding: 10px;
+  border-radius: 15px;
+  background: #e0e0e0;
+  box-shadow:  6px  6px 11px #bebebe,
+              -6px -6px 11px #ffffff;
+              
+  transition: box-shadow 150ms;
+
+  
+  ${props.isClickable && `
+    cursor: pointer;
+    
+    ${cssCardViewHover}
+
+    ${cssCardViewActive}
+  `}
+`
--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,47 +1,31 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
+import { cssCardView } from './CardView.style'
 
 interface Props {
   children?: React.ReactNode,
-  clickable?: boolean,
+  isClickable?: boolean,
+  isDisabled?: boolean,
+  isLoading?: boolean,
   onClick?: (e: React.MouseEvent) => void
 }
 
-const CardView: React.FC<Props> = ({ children, clickable, onClick }) => {
-  const CardCss = css`
-    width: 100%;
-    min-height: 100px;
-    padding: 10px;
-    border-radius: 15px;
-    background: #e0e0e0;
-    box-shadow:  6px  6px 11px #bebebe,
-                -6px -6px 11px #ffffff;
-                
-    transition: box-shadow 150ms;
-
-    &:hover {
-      box-shadow:  10px  10px 20px #bebebe,
-                  -10px -10px 20px #ffffff;
-    }
-    
-    ${clickable && `
-      cursor: pointer;
-
-      &:active:not(:focus-within) {
-        box-shadow: inset  7px  7px 14px #bebebe,
-                    inset -7px -7px 14px #ffffff;
-      }
-    `}
-  `;
+const CardView: React.FC<Props> = ({ children, isClickable, isDisabled, onClick }) => {
 
   const _onClick = (e: React.MouseEvent) => {
+    if (!isClickable || isDisabled) return
+
     e.preventDefault()
     e.stopPropagation()
     onClick && onClick(e)
   }
 
   return (
-    <div css={CardCss} onClick={_onClick}>
+    <div 
+      css={cssCardView({
+        isClickable: isClickable ?? false,
+        isDisabled: isDisabled ?? false,
+      })} 
+      onClick={_onClick}>
       { children }
     </div>
   )
