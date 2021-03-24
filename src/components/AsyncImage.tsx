60d3040c51dd1c6925d45fa095dc204ec387950a
hello-wahyu <al.arudei01@gmail.com>
2021-03-24 19:51:11 +0700

    Impl'd. AsyncImage

---
+++ src/components/AsyncImage.tsx
@@ -0,0 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import React, { useEffect, useState } from "react"
+
+interface Props {
+  src?: string,
+  alt?: string,
+  children: React.ReactNode,
+}
+
+const AsyncImage: React.FC<Props> = ({ src, children, ...props }) => {
+  const [loadedSrc, setLoadedSrc] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!src) return
+
+    const handleImageLoad = (e: Event) => {
+      setLoadedSrc(src)
+    }
+
+    const image = new Image()
+    image.src = src
+    image.addEventListener('load', handleImageLoad)
+
+    return () => {
+      image.removeEventListener('load', handleImageLoad)
+    }
+
+  }, [src])
+
+  const ImgCss = css`
+    width: 100%;
+    height: 100%;
+  `
+
+  if (src === loadedSrc) return (
+    <img css={ImgCss} src={src} {...props}/>
+  )
+  else {
+    return <>{children}</>
+  }
+}
+
+export { AsyncImage }
