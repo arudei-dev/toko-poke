25622d52b08e4b95bea5120a7f0bceb16b5f806f
hello-wahyu <al.arudei01@gmail.com>
2021-03-25 13:15:34 +0700

    Cleanup

--- src/components/PageBase.tsx
+++ src/components/PageBase.tsx
@@ -1,5 +1,4 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 import { useEffect, useRef } from 'react';
 import { useLocation } from "react-router-dom";
 import { cssPageBase, ThemeStyle } from './PageBase.style'
