1ff0606a23d6bd0bf53c1bb06283c799bb71b558
hello-wahyu <al.arudei01@gmail.com>
2021-03-25 12:13:02 +0700

    Impl'd. props: AutoScrollRestore

--- src/components/PageBase.tsx
+++ src/components/PageBase.tsx
@@ -1,16 +1,35 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { useEffect, useRef } from 'react';
+import { useLocation } from "react-router-dom";
 import { cssPageBase, ThemeStyle } from './PageBase.style'
 
 interface Props {
   noYScrolling?: boolean,
+  autoScrollRestore?: boolean
   children?: React.ReactNode,
-  themeStyle?: ThemeStyle
+  themeStyle?: ThemeStyle,
 }
 
-const PageBase: React.FC<Props> = ({ children, noYScrolling, themeStyle }) => {
+const PageBase: React.FC<Props> = ({ 
+  children, 
+  noYScrolling, 
+  autoScrollRestore,
+  themeStyle 
+}) => {
+
+  const location = useLocation();
+
+  const rootDivRef = useRef<HTMLDivElement>(null)
+  
+  useEffect(() => {
+    if (autoScrollRestore) {
+      rootDivRef?.current?.scrollTo(0,0)
+    }
+  }, [location.pathname, location.search])
+
   return (
-    <div css={cssPageBase({
+    <div ref={rootDivRef} css={cssPageBase({
       noYScrolling: noYScrolling ?? false,
       ThemeStyle: themeStyle ?? "light"
     })}>
