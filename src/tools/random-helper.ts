0933b268bbf5c18d419b07d3e60af34840a1b9c1
hello-wahyu <al.arudei01@gmail.com>
2021-03-25 07:48:24 +0700

    Impl'd. random-helper (toss a coin, roll a dice)

---
+++ src/tools/random-helper.ts
@@ -0,0 +1,21 @@
+
+enum CoinFace {
+  Head = 1,
+  Tails = 0,
+}
+
+function tossACoin(nudgeProbability: number = 0.5): CoinFace {
+  return Math.random() < nudgeProbability ? CoinFace.Head : CoinFace.Tails
+}
+
+function rollADice(): number {
+  const diceFace = [1,2,3,4,5,6]
+
+  const random = Math.floor(Math.random() * diceFace.length);
+  return diceFace[random]
+}
+
+export {
+  tossACoin,
+  rollADice
+}
