16507b469fa73d0a64526c02f5ec0b855853f834
hello-wahyu <al.arudei01@gmail.com>
2021-03-25 07:29:26 +0700

    New prop: disable y-scrolling (useful if content shown are skeleton-esque)

--- src/components/PageBase.tsx
+++ src/components/PageBase.tsx
@@ -2,15 +2,16 @@
 import { css } from '@emotion/react'
 
 interface Props {
+  noYScrolling?: boolean,
   children?: React.ReactNode,
 }
 
-const PageBase: React.FC<Props> = ({ children }) => {
+const PageBase: React.FC<Props> = ({ children, noYScrolling }) => {
   const PageCss = css`
     background-color: #e0e0e0;
     width: 100%;
     height: 100%;
-    overflow-y: auto;
+    overflow-y: ${noYScrolling ? css`none` : css`auto`};
   `;
 
   return (
