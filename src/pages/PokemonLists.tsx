d5c756d34a26fff392fda2811d4add554d0356ef
hello-wahyu <al.arudei01@gmail.com>
2021-03-26 07:40:37 +0700

    Laundry list of changes and stabilizations - Now using LazyQuery to handle paginations without total refresh; - Page is now a proper state and not just a hack; - Impl'd. previous page and next page button; - Page now properly restore scrolls when moving to a new page; - Going to PokemonDetails now take name instead of id (per request); - Stabilizations and cleanup (layout and logic).

--- src/pages/PokemonLists.tsx
+++ src/pages/PokemonLists.tsx
@@ -1,54 +1,94 @@
-import React, { useEffect } from 'react'
-import { useHistory, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { useHistory, useLocation } from "react-router-dom";
+import { useLQPokemonLists } from "core/services/pokemon-lists.hook";
+import { useRootState } from 'core/rootContext';
+import { parseUrlQuery } from 'tools/url-helper';
 import { GridView } from 'components/GridView'
 import { PageBase } from 'components/PageBase'
-import { useQPokemonLists } from "core/services/pokemon-lists.hook";
 import { PokemonCard } from 'components/PokemonCard'
+import { Button } from 'components/Button';
 import './PokemonLists.scss'
-import { parseUrlQuery } from 'tools/url-helper';
 
 const ITEM_LIMIT = 20;
-
-function PokemonLists() {
-
-  let history = useHistory();
-  const urlQueries = parseUrlQuery(history.location.search ?? "")
-
-  let page = urlQueries["page"] && parseInt(urlQueries["page"]) || 0;
-      page = page > 0 ? (page - 1) : 0;
-
-  let { data: pokeData, loading, error } = useQPokemonLists({ 
-    limit: ITEM_LIMIT, offset: (page * ITEM_LIMIT) 
-  })
-
   // - check if pokeData.next is null (out of bounds)
   // - cache max-count inside our context state,
   //   and update each reload.
-  
-  useEffect(() => {
 
-    console.log(page)
-  }, [loading])
-  
-  const onPokemonCardClick = (e: React.MouseEvent, id: number) => {
-    history.push(`/pokemon/${id}/details`);
+
+const PokemonLists = () => {
+  const { useTheme } = useRootState()
+
+  const history = useHistory()
+  const location = useLocation()
+  const urlQueries = parseUrlQuery(history.location.search ?? "")
+  const [page, setPage] = useState(0)
+
+  const [fetchPokeLists, {data: pokeData, loading, called}] = useLQPokemonLists()
+
+  useEffect(() => {
+    const currentPage = urlQueries["page"] 
+
+    // Check if the page query is valid, a number & > 0.
+    if (!currentPage || (parseInt(urlQueries["page"]) || 0) < 1) {
+      history.push({
+        pathname: `/`,
+        search: `?page=1`
+      })
+    }
+
+    setPage(parseInt(urlQueries["page"]))
+    window.scrollTo(0, 0);
+  }, [history, location.search, location.pathname])
+
+  useEffect(() => {
+    fetchPokeLists({
+      variables: {
+        limit: ITEM_LIMIT, offset: ((page - 1) * ITEM_LIMIT) 
+      }
+    })
+  }, [page])
+
+  const _onPokemonCardClick = (e: React.MouseEvent, name: string) => {
+    history.push(`/pokemon/${name}/details`);
   }
 
-  const renderComplete = () => (
+  const _onPreviousPageClick = (e: React.MouseEvent) => {
+    const prevPage = page - 1
+
+    history.push({
+      pathname: `/`,
+      search: `?page=${prevPage}`
+    })
+    // setPage(page - 1)
+  }
+
+  const _onNextPageClick = (e: React.MouseEvent) => {
+    const nextPage = page + 1
+
+    history.push({
+      pathname: `/`,
+      search: `?page=${nextPage}`
+    })
+    // setPage(page + 1)
+  }
+
+  const _renderComplete = () => (
     pokeData?.results?.map(poke => (
-      <PokemonCard
+      <PokemonCard 
+        themeStyle={useTheme}
         key={poke?.id!}
         id={poke?.id!}
         pokeName={poke?.name!} 
         pokeSpriteURL={poke?.image!}
         autoCapitalize={true}
-        onClick={(e) => onPokemonCardClick(e, poke?.id!)}/>
+        onClick={(e) => _onPokemonCardClick(e, poke?.name!)}/>
     ))
   )
 
-  const renderLoading = () => (
+  const _renderLoading = () => (
     Array.from({length: 20}, (_, i) => (
       <PokemonCard
+        themeStyle={useTheme}
         key={i}
         id={i}
         usePlaceholder={true}
@@ -57,16 +97,31 @@ function PokemonLists() {
   )
 
   return (
-    <PageBase noYScrolling={loading}>
-      <div className="PagePokemonLists">
-        <div className="page-header">
-          Gotta catch 'em all!
-        </div>
+    <PageBase 
+      autoScrollRestore={true}
+      noYScrolling={loading} 
+      themeStyle={useTheme} 
+      >
+      <div className="page-pokemon-lists">
         <GridView usePadding={true}>
           {
-            loading ? renderLoading() : renderComplete()
+            (loading || !called)  ? _renderLoading() : _renderComplete()
           }
         </GridView>
+        <div className="view-actions">
+          <Button
+            themeStyle={useTheme}
+            isDisabled={page < 2 && true}
+            title="Previous page"
+            onClick={_onPreviousPageClick}
+            />
+          <div className="separator"/>
+          <Button
+            themeStyle={useTheme}
+            title="Next page"
+            onClick={_onNextPageClick}
+            />
+        </div>
       </div>
     </PageBase>
   )
