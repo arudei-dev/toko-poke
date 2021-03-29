b93716fe47bbd92c3ba1d1f6717915fa34493268
Wahyu <al.arudei01@gmail.com>
2021-03-29 12:07:54 +0700

    Refactored PokemonBio into separate layout component

---
+++ src/app/layouts/PokemonDetails/PokemonBio.style.tsx
@@ -0,0 +1,130 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { ThemeAwareLayout, mqSizes } from 'components/theme'
+
+
+export const cssLayoutPokemonBio = () => css`
+  width: 100%;
+
+
+  .poke-bio {
+    width: 100%;
+    height: 100%;
+    padding-bottom: 16px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media (max-width: ${mqSizes.desktopM}) {
+      width: 100%;
+      height: 100%;
+      padding-bottom: 0;
+      padding-right: 16px;
+
+      display: grid;
+      grid-template-columns: minmax(0, auto) minmax(0, 1fr) !important;
+      grid-template-rows: 1fr;
+      gap: 0px 0px;
+      grid-template-areas: ". .";
+    }
+
+    @media (max-width: ${mqSizes.mobileM}) {
+      padding-bottom: 8px;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .poke-avatar {
+      width: 200px;
+      height: 200px;
+      image-rendering: pixelated;
+
+      @media (max-width: ${mqSizes.desktopS}) {
+        width: 150px;
+        height: 150px;
+      }
+
+      @media (max-width: ${mqSizes.mobileL}) {
+        width: 100px;
+        height: 100px;
+      }
+
+      @media (max-width: ${mqSizes.mobileM}) {
+        width: 150px;
+        height: 150px;
+      }
+    }
+
+    .poke-details {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      @media (max-width: ${mqSizes.desktopM}) {
+        padding-left: 8px;
+        align-items: flex-start;
+      }
+      
+      @media (max-width: ${mqSizes.mobileM}) {
+        align-items: center;
+      }
+      
+      .poke-infohero {
+        color: white;
+        font-family: "Ubuntu";
+        font-size: 36pt;
+        font-weight: 600;
+
+        @media (max-width: ${mqSizes.desktopS}) {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          
+          font-size: 32pt;
+        }
+
+        @media (max-width: ${mqSizes.mobileL}) {
+          font-size: 22pt;
+        }
+
+        @media (max-width: ${mqSizes.mobileM}) {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          
+          font-size: 32pt;
+        }
+      }
+
+      .poke-types {
+        width: 100%;
+        // padding-top: 8px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .poke-types-list {
+          width: 100%;
+          padding-top: 4px;
+
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          justify-content: center;
+
+          @media (max-width: ${mqSizes.desktopM}) {
+            justify-content: flex-start;
+          }
+
+          @media (max-width: ${mqSizes.mobileM}) {
+            justify-content: center;
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/app/layouts/PokemonDetails/PokemonBio.tsx
@@ -0,0 +1,83 @@
+/** @jsxImportSource @emotion/react */
+import { PokemonDetailsByName_pokemon } from 'context/Apollo/types/PokemonDetailsByName'
+import { capitalizeEachWord } from 'core/utils/string-helper'
+import { 
+  AsyncImage,
+  Text, 
+  InfoChip 
+} from 'components'
+import { PokeSilhouette } from 'assets/svg/PokeSilhouette'
+import { cssLayoutPokemonBio } from './PokemonBio.style'
+
+
+interface Props {
+  pokeData?: PokemonDetailsByName_pokemon
+}
+
+export const LayoutPokemonBio: React.FC<Props> = ({
+  pokeData,
+}) => {
+  const themeStyle = 'light'
+
+  const pokeName = "charizard"
+
+  return (
+    <div
+      css={cssLayoutPokemonBio()}
+      >
+      
+      <div className="poke-bio">
+
+        <div className="poke-avatar">
+          {
+            pokeData?.sprites?.front_default ? (
+              <AsyncImage src={pokeData.sprites.front_default} alt={`Pokemon '${pokeName}'`}>
+                <PokeSilhouette color="#00000033"/>
+              </AsyncImage>
+            ) : (
+              <PokeSilhouette color="#00000011"/>
+            )
+          }
+        </div>
+
+        <div className="poke-details">
+          <div className="poke-infohero">
+            <Text 
+              text={capitalizeEachWord(pokeData?.name || "???")}
+              textColor='primary'
+              themeStyle={themeStyle}
+              />
+          </div>
+
+          <div className="poke-types">
+            <div className="poke-types-list">
+              {
+                pokeData?.types?.map((type, idx) => (
+                  type?.type?.name ? (
+                    <InfoChip 
+                      key={`pokeType-${type}-${idx}`}
+                      size="small"
+                      title={type.type.name}
+                      themeStyle={themeStyle}
+                      />
+                  ) : <></>
+                ))
+              }
+              {
+                pokeData?.weight && (
+                  <InfoChip 
+                    size="small"
+                    title={`Weight: ${pokeData?.weight / 10.0} kg`}
+                    themeStyle={themeStyle}
+                    />
+                )
+              }
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
