3de9a78400ef1cadcb47f79dfc38257ab22138fe
Wahyu <al.arudei01@gmail.com>
2021-03-29 15:22:20 +0700

    Impl'd. string truncate with ellipsis

--- src/core/utils/string-helper.ts
+++ src/core/utils/string-helper.ts
@@ -8,6 +8,18 @@ function capitalize1stLetterOfEachWord(s: string): string {
   ).join(" ");
 }
 
+function truncateWithEllipsis(s: string, maxLength: number, wordBound = true): string {
+  if (s.length <= maxLength)
+    return s
+
+  const substring = s.substr(0, maxLength + 1)
+
+  return (wordBound
+    ? substring.substr(0, substring.lastIndexOf(" "))
+    : substring) + "..."
+}
+
 export {
-  capitalize1stLetterOfEachWord
+  capitalize1stLetterOfEachWord,
+  truncateWithEllipsis
 }
