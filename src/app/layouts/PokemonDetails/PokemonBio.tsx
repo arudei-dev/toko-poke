fa2c64e78a1cf29cca70d73426068b7b2fa0b097
Wahyu <al.arudei01@gmail.com>
2021-03-29 14:21:03 +0700

    Impl'd. Placeholder

--- src/app/layouts/PokemonDetails/PokemonBio.tsx
+++ src/app/layouts/PokemonDetails/PokemonBio.tsx
@@ -12,12 +12,14 @@ import { ThemeAwareLayout } from 'components/theme'
 
 
 interface Props extends Partial<ThemeAwareLayout> {
-  pokeData?: PokemonDetailsByName_pokemon
+  pokeData?: PokemonDetailsByName_pokemon,
+  usePlaceholder?: boolean
 }
 
 export const LayoutPokemonBio: React.FC<Props> = ({
   pokeData,
   themeStyle,
+  usePlaceholder,
 }) => {
 
   const pokeName = "charizard"
@@ -33,10 +35,12 @@ export const LayoutPokemonBio: React.FC<Props> = ({
 
         <div className="poke-avatar">
           {
-            pokeData?.sprites?.front_default ? (
-              <AsyncImage src={pokeData.sprites.front_default} alt={`Pokemon '${pokeName}'`}>
-                <PokeSilhouette color="#00000033"/>
-              </AsyncImage>
+            !usePlaceholder ? (
+              (pokeData?.sprites?.front_default) && (
+                <AsyncImage src={pokeData?.sprites?.front_default} alt={`Pokemon '${pokeName}'`}>
+                  <PokeSilhouette color="#00000033"/>
+                </AsyncImage>
+              ) 
             ) : (
               <PokeSilhouette color="#00000011"/>
             )
@@ -45,36 +49,56 @@ export const LayoutPokemonBio: React.FC<Props> = ({
 
         <div className="poke-details">
           <div className="poke-infohero">
-            <Text 
-              text={capitalize1stLetterOfEachWord(pokeData?.name || "???")}
-              textColor='primary'
-              themeStyle={themeStyle}
-              />
+            {
+              !usePlaceholder ? (
+                <Text 
+                  text={capitalize1stLetterOfEachWord(pokeData?.name || "???")}
+                  textColor='primary'
+                  themeStyle={themeStyle}
+                  />
+              ) : (
+                <Text 
+                  text={"Loading..."}
+                  textColor='disabled'
+                  themeStyle={themeStyle}
+                  />
+              )
+            }
           </div>
 
           <div className="poke-types">
             <div className="poke-types-list">
               {
-                pokeData?.types?.map((type, idx) => (
-                  type?.type?.name ? (
-                    <InfoChip 
-                      key={`pokeType-${type}-${idx}`}
-                      size="small"
-                      title={type.type.name}
-                      themeStyle={themeStyle}
-                      />
-                  ) : <></>
-                ))
-              }
-              {
-                pokeData?.weight && (
-                  <InfoChip 
-                    size="small"
-                    title={`Weight: ${pokeData?.weight / 10.0} kg`}
-                    themeStyle={themeStyle}
-                    />
+                !usePlaceholder ? (
+                  <>
+                  {
+                    pokeData?.types?.map((type, idx) => (
+                      type?.type?.name ? (
+                        <InfoChip 
+                          key={`pokeType-${type}-${idx}`}
+                          size="small"
+                          title={type.type.name}
+                          themeStyle={themeStyle}
+                          />
+                      ) : <></>
+                    ))
+                  }
+                  {
+                    pokeData?.weight && (
+                      <InfoChip 
+                        size="small"
+                        title={`Weight: ${pokeData?.weight / 10.0} kg`}
+                        themeStyle={themeStyle}
+                        />
+                    )
+                  }
+                  </>
+                ) : (
+                  <>
+                  </>
                 )
               }
+              
             </div>
           </div>
         </div>
